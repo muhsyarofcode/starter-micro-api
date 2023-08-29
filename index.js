88f23031e62eeeb96eebb32cee00cd876da8f41f
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-29 11:19:22 +0700

    !

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ try {
 
 app.use(cors({origin:"http://localhost:3000", credentials:true, whithCredentials:true}));
 app.use(passport.initialize())
-app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded());
 app.use(session({
   secret: 'keyboard cat',
