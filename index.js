b2d8a67a6deb5447a71ff7f2357885f1adecee9a
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-29 15:03:23 +0700

    !!

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ router.get('/oauth2/redirect/google', passport.authenticate('google', {
   failureRedirect:'/'
 }));
 router.get('/setcookie', function(req,res){
-  res.json(req.session.passport.user.profile)
+  res.json(req.session.passport.user)
 });
 router.get('/token', refreshToken);
 router.delete('/out', Logout);
