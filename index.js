57c313115d25e7afb39cb43ed685e6f332c50788
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-29 16:57:58 +0700

    !!!

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ router.post('/login', Login);
 router.get('/loginGoogle', passport.authenticate('google', {scope: ["profile", "email"]}));
 router.get('/oauth2/redirect/google', passport.authenticate('google', {
   successReturnToOrRedirect:"http://localhost:3000/connect/setcookie",
-  successFlash: "profile.name",
+  successMessage: "profile.name",
   failureRedirect:"http://localhost:3000/connect"
 }));
 router.get('/token', refreshToken);
