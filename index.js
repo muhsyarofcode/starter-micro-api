25dcfc2cdc7baa72df5a4349b46725498382252d
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-29 12:57:58 +0700

    redirect 3000, with secure

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie:{
-    httpOnly: true,
+    secure:true,
     maxAge: 24 * 60 * 60 * 1000
   }
 }));
@@ -98,7 +98,7 @@ router.get('/oauth2/redirect/google', passport.authenticate('google', {
   successReturnToOrRedirect:'http://localhost:3000/connect/setcookie',
   failureRedirect:'/'
 }));
-router.get('/setcookie',passport.authenticate('session'), function(req,res){
+router.get('/setcookie', function(req,res){
   console.log(req.session)
 });
 router.get('/token', refreshToken);
