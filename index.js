7653a78a2efa4c10c270415a1a20aaef1791a24a
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-29 13:26:17 +0700

    !!

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ router.get('/oauth2/redirect/google', passport.authenticate('google', {
 }));
 router.get('/setcookie', function(req,res){
   res.json(req.session)
-  res.cookie("refreshToken", req.session.passport.user.refreshToken)
+  res.cookie("cookie", req.session)
 });
 router.get('/token', refreshToken);
 router.delete('/out', Logout);
