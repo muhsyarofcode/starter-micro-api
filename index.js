87a1cf31d1daf456fc17e219bdb3da7641a147dd
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-01 20:43:22 +0700

    ok we get all data profile

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ passport.use('facebook',new FacebookStrategy.Strategy({
   profileFields: [ 'email' , 'name', 'id', 'picture' ]
 },
 async function(req, accessToken, profile, done) {
-console.log(profile.emails[0].value)
+console.log(profile)
   return done(null, profile);
 }
   ));
