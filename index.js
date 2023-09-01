ac40d8d941e408a777e8724ddcd43baff964d522
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-01 17:28:02 +0700

    test 1

--- index.js
+++ index.js
@@ -63,12 +63,12 @@ passport.use('facebook',new FacebookStrategy.Strategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   callbackURL: 'https://ultramarine-hen-kilt.cyclic.app/oauth2/redirect/facebook',
-  scope: ['public_profile'],
+  scope: ['public_profile','email'],
   state: true
 },
-async function(req, accessToken, public_profile, done) {
-  console.log(public_profile)
-  return done(null, public_profile);
+async function(req, accessToken, email, public_profile, done) {
+  const profile = public_profile+email
+  return done(null, profile);
 }
   ));
 
