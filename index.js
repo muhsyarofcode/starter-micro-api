870285ec393f06e1fbfa22d5ff50e83984793108
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-01 19:42:39 +0700

    get a data

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ passport.use('facebook',new FacebookStrategy.Strategy({
   profileFields: [ 'email' , 'name', 'id', 'picture' ]
 },
 async function(req, accessToken, profile, done) {
-  console.log(profile.email)
+  console.log(profile[0].id)
   return done(null, profile);
 }
   ));
