5dd70cfb6e8c2ed283657829e9f131123df30a7d
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-01 19:53:44 +0700

    console.log user in serializeuser

--- index.js
+++ index.js
@@ -67,13 +67,13 @@ passport.use('facebook',new FacebookStrategy.Strategy({
   profileFields: [ 'email' , 'name', 'id', 'picture' ]
 },
 async function(req, accessToken, profile, done) {
-  console.log(profile[0].id)
   return done(null, profile);
 }
   ));
 
   
 passport.serializeUser((user, done) => {
+  console.log(user)
   return done(null, (user));
 });
 
