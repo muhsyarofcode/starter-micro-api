0e7df89954f37c817a11d46dbdc6f4cc3324c98f
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-04 19:04:27 +0700

    no set charset

--- config/Database.js
+++ config/Database.js
@@ -2,8 +2,7 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('bsfbrh0ikechnlvntabc','ucjdmyeitlkko9sd','30tOMRbk7Lj4Uobnb6xH', {
     host: "bsfbrh0ikechnlvntabc-mysql.services.clever-cloud.com",
-    dialect: "mysql",
-    charset: "utf8mb4"
+    dialect: "mysql"
 })
 
 export default db;
