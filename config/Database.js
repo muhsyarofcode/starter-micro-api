4cf21846a9cbb4464535150ead15f64f1dde974e
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-04 19:02:55 +0700

    emoji for message

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('bsfbrh0ikechnlvntabc','ucjdmyeitlkko9sd','30tOMRbk7Lj4Uobnb6xH', {
     host: "bsfbrh0ikechnlvntabc-mysql.services.clever-cloud.com",
-    dialect: "mysql"
+    dialect: "mysql",
+    charset: "utf8mb4"
 })
 
 export default db;
