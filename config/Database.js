a3517208e50c6e805cf9dcf698d4d75f095b5272
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-04 10:29:13 +0700

    connect db

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('muhsyar11_auth','muhsyar11_db','ImrJilTN4Rwi', {
+const db = new Sequelize('muhsyar11_auth','muhsyar11_db','5StjF8qWbRfo', {
     host: "103.112.244.90",
     dialect: "mysql"
 })
