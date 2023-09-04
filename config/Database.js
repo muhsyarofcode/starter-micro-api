d17f81dd73469ad3b7df759271ccfc7162b45d87
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-04 10:42:55 +0700

    use ip

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('if0_34823801_conncet_db','if0_34823801','hzhRlHgQjJ', {
-    host: "sql306.infinityfree.com",
+    host: "185.27.134.112",
     dialect: "mysql"
 })
 
