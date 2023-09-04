19e18370e4927147bbe7851791a02819f7a52923
MSH <125707401+muhsyarofcode@users.noreply.github.com>
2023-09-04 10:41:00 +0700

    change db

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('muhsyar11_auth','muhsyar11_db','5StjF8qWbRfo', {
-    host: "103.112.244.90",
+const db = new Sequelize('if0_34823801_conncet_db','if0_34823801','hzhRlHgQjJ', {
+    host: "sql306.infinityfree.com",
     dialect: "mysql"
 })
 
